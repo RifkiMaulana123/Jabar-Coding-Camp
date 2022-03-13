030d2a700adad3541a236e237525f2c067276936
RifkiMaulana123 <maulanfikri98@gmail.com>
2022-03-13 18:01:36 +0700

    Function penghitung jumlah kata

---
+++ Quiz-1/index.js
@@ -0,0 +1,11 @@
+//Soal No 1
+function jumlah_kata() {
+    var kalimat_1 = "Halo nama saya Muhammad Iqbal Mubarok".split(" ").length;
+    var kalimat_2 = "Saya Iqbal".split(" ").length;
+    var kalimat_3 = "Saya Muhammad Iqbal Mubarok".split(" ").length;
+    console.log("Jumlah Kata: " +kalimat_1)
+    console.log("Jumlah Kata: " +kalimat_2)
+    console.log("Jumlah Kata: " +kalimat_3)
+}
+
+jumlah_kata()
