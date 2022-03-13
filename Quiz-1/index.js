5e034f350a757571275e07fdd61631509830dfaf
RifkiMaulana123 <maulanfikri98@gmail.com>
2022-03-13 18:04:40 +0700

    Function Penghasil Tanggal Hari Esok

--- Quiz-1/index.js
+++ Quiz-1/index.js
@@ -9,3 +9,36 @@ function jumlah_kata() {
 }
 
 jumlah_kata()
+
+// Soal 2 Contoh 1
+function next_date(tanggal, bulan, tahun) {
+    var tanggal = 29
+    var bulan = 2
+    var tahun = 2020
+    var tglnext = new Date(tahun, bulan-1, tanggal+1);
+    var format = {year: 'numeric', month: 'long', day: 'numeric' };
+    console.log('\n'+tglnext.toLocaleDateString("id", format));
+}
+next_date()
+
+// Soal 2 Contoh 2
+function next_date2(tanggal, bulan, tahun) {
+    var tanggal = 28
+    var bulan = 2
+    var tahun = 2021
+    var tglnext = new Date(tahun, bulan-1, tanggal+1);
+    var format = {year: 'numeric', month: 'long', day: 'numeric' };
+    console.log(tglnext.toLocaleDateString("id", format));
+}
+next_date2()
+
+// Soal 2 Contoh 1
+function next_date3(tanggal, bulan, tahun) {
+    var tanggal = 31
+    var bulan = 12
+    var tahun = 2020
+    var tglnext = new Date(tahun, bulan-1, tanggal+1);
+    var format = {year: 'numeric', month: 'long', day: 'numeric' };
+    console.log(tglnext.toLocaleDateString("id", format));
+}
+next_date3()
